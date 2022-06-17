5090f45fef69cd4ea46b6bcc6f0ffbda73be5a27
Mauritz Langer <mauritz@langer-net.de>
2022-06-17 13:54:04 +0200

    integration of some weather, co2, population and water diagrams from tableau

--- src/app/pages/co2/co2.component.ts
+++ src/app/pages/co2/co2.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var tableau: any;
+
 @Component({
   selector: 'co2Component',
   moduleId: module.id,
@@ -7,6 +9,48 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class Co2Component implements OnInit{
+
+  co2OverTheYears:any;
+  co2Weather:any;
+  co2PerHeadPerYear:any;
+  co2PerHeadPerYearPerCountry:any;
+
   ngOnInit(){
+    this.drawCo2OverTheYears();
+    this.drawWeatherCo2();
+  }
+
+  drawCo2OverTheYears(){
+    let placeholderDiv = document.getElementById('co2OverTheYears');
+    // Replace this url with the url of your Tableau dashboard
+    let url = 'https://public.tableau.com/views/WetterAPI/CO2_1?:language=en-US&:display_count=n&:origin=viz_share_link&:toolbar=no&:showShareOptions=false';
+    let options = {
+      hideTabs: true,
+      width: "100%",
+      height: "800px",
+      onFirstInteractive: function() {
+        // The viz is now ready and can be safely used.
+        console.log("Run this code when the viz has finished     loading.");
+      }
+    };
+    // Creating a viz object and embed it in the container div.
+    this.co2OverTheYears = new tableau.Viz(placeholderDiv, url, options);
+  }
+
+  drawWeatherCo2(){
+    let placeholderDiv = document.getElementById('co2Weather');
+    // Replace this url with the url of your Tableau dashboard
+    let url = 'https://public.tableau.com/views/WetterAPI/Wetter_CO2?:language=en-US&:display_count=n&:origin=viz_share_link&:toolbar=no&:showShareOptions=false';
+    let options = {
+      hideTabs: true,
+      width: "100%",
+      height: "800px",
+      onFirstInteractive: function() {
+        // The viz is now ready and can be safely used.
+        console.log("Run this code when the viz has finished     loading.");
+      }
+    };
+    // Creating a viz object and embed it in the container div.
+    this.co2Weather = new tableau.Viz(placeholderDiv, url, options);
   }
 }
