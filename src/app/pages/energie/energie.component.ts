7a56984edec2757fdfdb656a1cfbe17097b5f5fe
Armann Grewal <grewal.armann15@gmail.com>
2022-06-23 15:55:23 +0200

    adding graphs of the last 6 Topics

--- src/app/pages/energie/energie.component.ts
+++ src/app/pages/energie/energie.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var tableau: any;
+
 @Component({
   selector: 'energieComponent',
   moduleId: module.id,
@@ -8,5 +10,57 @@ import { Component, OnInit } from '@angular/core';
 
 export class EnergieComponent implements OnInit{
   ngOnInit(){
+    this.drawEnergyOverTheYears()
+    this.drawElectricityOverTheYears()
+    this.drawEnergyTempOverTheYears()
+  }
+  drawEnergyOverTheYears(){
+    let placeholderDiv = document.getElementById('energyOverTheYears');
+    // Replace this url with the url of your Tableau dashboard
+    let url = 'https://public.tableau.com/views/WetterAPI/AnteileEE?:language=de-DE&:display_count=n&:origin=viz_share_link';
+    let options = {
+      hideTabs: true,
+      width: "100%",
+      height: "800px",
+      onFirstInteractive: function() {
+        // The viz is now ready and can be safely used.
+        console.log("Run this code when the viz has finished     loading.");
+      }
+    };
+    // Creating a viz object and embed it in the container div.
+    this.drawEnergyOverTheYears = new tableau.Viz(placeholderDiv, url, options);
+  }
+
+  drawElectricityOverTheYears(){
+    let placeholderDiv = document.getElementById('electricityOverTheYears');
+    // Replace this url with the url of your Tableau dashboard
+    let url = 'https://public.tableau.com/views/WetterAPI/Energy_doppelt?:language=de-DE&:display_count=n&:origin=viz_share_link';
+    let options = {
+      hideTabs: true,
+      width: "100%",
+      height: "800px",
+      onFirstInteractive: function() {
+        // The viz is now ready and can be safely used.
+        console.log("Run this code when the viz has finished     loading.");
+      }
+    };
+    // Creating a viz object and embed it in the container div.
+    this.drawElectricityOverTheYears = new tableau.Viz(placeholderDiv, url, options);
+  }
+  drawEnergyTempOverTheYears(){
+    let placeholderDiv = document.getElementById('energyTempOverTheYears');
+    // Replace this url with the url of your Tableau dashboard
+    let url = 'https://public.tableau.com/views/WetterAPI/TEST_ENERGY-X?:language=de-DE&:display_count=n&:origin=viz_share_link';
+    let options = {
+      hideTabs: true,
+      width: "100%",
+      height: "800px",
+      onFirstInteractive: function() {
+        // The viz is now ready and can be safely used.
+        console.log("Run this code when the viz has finished     loading.");
+      }
+    };
+    // Creating a viz object and embed it in the container div.
+    this.drawEnergyTempOverTheYears = new tableau.Viz(placeholderDiv, url, options);
   }
 }
--- src/app/pages/internet/internet.component.ts
+++ src/app/pages/internet/internet.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var tableau: any;
+
 @Component({
   selector: 'internetComponent',
   moduleId: module.id,
@@ -8,5 +10,39 @@ import { Component, OnInit } from '@angular/core';
 
 export class InternetComponent implements OnInit{
   ngOnInit(){
+    this.drawInternetOverTheYears();
+    this.drawInternetCo2OverTheYears();
+  }
+  drawInternetOverTheYears(){
+    let placeholderDiv = document.getElementById('internetOverTheYears');
+    // Replace this url with the url of your Tableau dashboard
+    let url = 'https://public.tableau.com/views/WetterAPI/Internet_1?:language=de-DE&:display_count=n&:origin=viz_share_link';
+    let options = {
+      hideTabs: true,
+      width: "100%",
+      height: "800px",
+      onFirstInteractive: function() {
+        // The viz is now ready and can be safely used.
+        console.log("Run this code when the viz has finished     loading.");
+      }
+    };
+    // Creating a viz object and embed it in the container div.
+    this.drawInternetOverTheYears = new tableau.Viz(placeholderDiv, url, options);
+  }
+  drawInternetCo2OverTheYears(){
+    let placeholderDiv = document.getElementById('internetCo2OverTheYears');
+    // Replace this url with the url of your Tableau dashboard
+    let url = 'https://public.tableau.com/views/WetterAPI/INTERNET_CO2?:language=de-DE&:display_count=n&:origin=viz_share_link';
+    let options = {
+      hideTabs: true,
+      width: "100%",
+      height: "800px",
+      onFirstInteractive: function() {
+        // The viz is now ready and can be safely used.
+        console.log("Run this code when the viz has finished     loading.");
+      }
+    };
+    // Creating a viz object and embed it in the container div.
+    this.drawInternetCo2OverTheYears = new tableau.Viz(placeholderDiv, url, options);
   }
 }
