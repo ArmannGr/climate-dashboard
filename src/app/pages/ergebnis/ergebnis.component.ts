c9d2feb37b768c69a626b1ecf2f9f6c22eeb1c03
Armann Grewal <grewal.armann15@gmail.com>
2022-07-05 20:00:26 +0200

    Changes by alexa

--- src/app/pages/ergebnis/ergebnis.component.ts
+++ src/app/pages/ergebnis/ergebnis.component.ts
@@ -11,6 +11,24 @@ declare var tableau: any;
 export class ErgebnisComponent implements OnInit{
 
   ngOnInit(){
+    this.drawErgebnis();
+  }
+
+  drawErgebnis(){
+    let placeholderDiv = document.getElementById('drawErgebnis');
+    // Replace this url with the url of your Tableau dashboard
+    let url = 'https://public.tableau.com/shared/TQZ3PQ76Z?:display_count=n&:origin=viz_share_link';
+    let options = {
+      hideTabs: true,
+      width: "100%",
+      height: "800px",
+      onFirstInteractive: function() {
+        // The viz is now ready and can be safely used.
+        console.log("Run this code when the viz has finished     loading.");
+      }
+    };
+    // Creating a viz object and embed it in the container div.
+    this.drawErgebnis = new tableau.Viz(placeholderDiv, url, options);
   }
 
 
